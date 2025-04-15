2d8f8c688b241208c8aa23a3e31c30ecc8ad4316
Felipe Campos <felipe.campos@implantainformatica.com.br>
2025-04-15 11:53:08 -0300

    Finalizando serviço de produtos

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -0,0 +1,48 @@
+﻿using Marten.Schema;
+
+namespace Catalog.API.Data;
+
+public class CatalogInitialData : IInitialData
+{
+    public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+    {
+        using var session = store.LightweightSession();
+
+        if (await session.Query<Product>().AnyAsync())
+            return;
+
+        session.Store(GetPreconfiguredProducts());
+        await session.SaveChangesAsync();
+    }
+
+    private static IEnumerable<Product> GetPreconfiguredProducts() =>
+    [
+        new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "IPhone X",
+            Description = "This phone is the company's biggest change to its ...",
+            ImageFile = "product1.png",
+            Price = 950.00M,
+            Category = ["Smart Phone"]
+        },
+        new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Sansung 10",
+            Description = "This phone is the company's biggest change to its ...",
+            ImageFile = "product2.png",
+            Price = 840.00M,
+            Category = ["Smart Phone"]
+        },
+        new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Panasonic Lumix",
+            Description = "This phone is the company's biggest change to its ...",
+            ImageFile = "product3.png",
+            Price = 240.00M,
+            Category = ["Camera"]
+        }
+    ];
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,20 +1,48 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
+
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
+string connectionString = builder.Configuration.GetConnectionString("Database")!;
 
 builder.Services.AddMarten(opts =>
 {
-    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Connection(connectionString);
 }).UseLightweightSessions();
+
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(connectionString);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
